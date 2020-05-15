1fd3b8f052c56c81c0bcc10a5ff2909b367eeb9b
sineverba <alessandro.arici@gmail.com>
2020-05-15 11:04:41 +0200

    Fix Travis

---
+++ .deploy/scripts/basetest.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+VSC_REF=$(git rev-parse --short HEAD)
+
+echo VCS_REF
+echo $VCS_REF
+echo $$VCS_REF
+
+#docker run -d --name herodock -e "PORT=9876" -p 9876:9876 ${HEROKU_REGISTRY_IMAGE};
+#docker exec -it herodock php -i | grep "PHP Version => 7.4.5"
+#docker inspect -f '{{ .Config.Labels }}' herodock | grep $$VCS_REF
