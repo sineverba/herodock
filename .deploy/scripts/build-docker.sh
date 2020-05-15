330e99b8ef657632030b0060b445c7f26ce933b7
sineverba <alessandro.arici@gmail.com>
2020-05-15 08:12:26 +0200

    Add microbadger

--- .deploy/scripts/build-docker.sh
+++ .deploy/scripts/build-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build --tag $DOCKER_USERNAME/$HEROKU_APP_NAME --file ./.deploy/app/Dockerfile ".";
+docker build --tag $DOCKER_USERNAME/$HEROKU_APP_NAME --build-arg VCS_REF=`git rev-parse --short HEAD` --file ./.deploy/app/Dockerfile ".";
