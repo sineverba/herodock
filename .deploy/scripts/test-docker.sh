fde795c0b59c1295f2fa621edcae6b2979509ad1
sineverba <alessandro.arici@gmail.com>
2020-05-15 14:57:56 +0200

    Fix Travis

--- .deploy/scripts/test-docker.sh
+++ .deploy/scripts/test-docker.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-#VSC_REF=$(git rev-parse --short HEAD)
-
 docker run -d --name herodockdocker -e "PORT=9876" -p 9876:9876 $DOCKER_USERNAME/$HEROKU_APP_NAME:latest;
 docker exec -it herodockdocker php -i | grep "PHP Version => 7.4.5"
-# docker inspect -f '{{ .Config.Labels }}' herodockdocker | grep `git rev-parse --short HEAD`
 docker inspect -f '{{ .Config.Labels }}' herodockdocker
+docker inspect -f '{{ .Config.Labels }}' herodockdocker | grep $VCS_REF
