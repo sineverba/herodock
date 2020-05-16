3df67c34162262cef1214cc2dbf3322f12520fc7
sineverba <alessandro.arici@gmail.com>
2020-05-16 10:26:53 +0200

    Fix Travis doesnt stop on failing test

--- .deploy/scripts/test.sh
+++ .deploy/scripts/test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 docker run -d --name $HEROKU_APP_NAME -e "PORT=9876" -p 9876:9876 ${HEROKU_REGISTRY_IMAGE};
 docker exec -it $HEROKU_APP_NAME php -i | grep "PHP Version => 7.4.5"
+docker inspect -f {{.Config.Labels}} $HEROKU_APP_NAME
 docker inspect -f {{.Config.Labels}} $HEROKU_APP_NAME | grep $VCS_REF
