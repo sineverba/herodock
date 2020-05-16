0ef06aeb598c6956301c23882203d6e22c253478
sineverba <alessandro.arici@gmail.com>
2020-05-16 08:41:29 +0200

    Build for docker from Heroku tag

--- .deploy/scripts/build-docker.sh
+++ .deploy/scripts/build-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build --tag $DOCKER_USERNAME/$HEROKU_APP_NAME:latest \
-              --tag $DOCKER_USERNAME/$HEROKU_APP_NAME:$TRAVIS_TAG \
-              --build-arg VCS_REF=$VCS_REF \
-              --file ./.deploy/app/Dockerfile ".";
+#docker build --tag $DOCKER_USERNAME/$HEROKU_APP_NAME:latest \
+#              --tag $DOCKER_USERNAME/$HEROKU_APP_NAME:$TRAVIS_TAG \
+#              --build-arg VCS_REF=$VCS_REF \
+#              --file ./.deploy/app/Dockerfile ".";
--- .deploy/scripts/build.sh
+++ .deploy/scripts/build.sh
@@ -3,3 +3,7 @@
 docker build --tag ${HEROKU_REGISTRY_IMAGE} \
               --build-arg VCS_REF=$VCS_REF \
               --file ./.deploy/app/Dockerfile ".";
+
+# Tag also for docker
+docker tag ${HEROKU_REGISTRY_IMAGE} $DOCKER_USERNAME/$HEROKU_APP_NAME:latest
+docker tag ${HEROKU_REGISTRY_IMAGE} $DOCKER_USERNAME/$HEROKU_APP_NAME:$TRAVIS_TAG
