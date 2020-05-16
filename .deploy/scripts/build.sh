484f52daefaad299d882df435608c2ab00fbbdc4
sineverba <alessandro.arici@gmail.com>
2020-05-16 08:52:58 +0200

    Fix troubles

--- .deploy/scripts/build.sh
+++ .deploy/scripts/build.sh
@@ -4,6 +4,9 @@ docker build --tag ${HEROKU_REGISTRY_IMAGE} \
               --build-arg VCS_REF=$VCS_REF \
               --file ./.deploy/app/Dockerfile ".";
 
+docker images
 # Tag also for docker
 #docker tag ${HEROKU_REGISTRY_IMAGE} "$DOCKER_USERNAME/$HEROKU_APP_NAME":latest
-docker tag ${HEROKU_REGISTRY_IMAGE} "$DOCKER_USERNAME/$HEROKU_APP_NAME":"$TRAVIS_TAG"
+#docker tag ${HEROKU_REGISTRY_IMAGE} "$DOCKER_USERNAME/$HEROKU_APP_NAME":"$TRAVIS_TAG"
+docker tag ${HEROKU_REGISTRY_IMAGE} "sineverba/herodock:a1b2c3"
+docker images
