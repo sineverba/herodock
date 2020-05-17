7772ba4ac4b8770fd013b937b850759b7a55d679
sineverba <alessandro.arici@gmail.com>
2020-05-17 07:50:06 +0200

    Fix PHP Version

--- .deploy/scripts/test.sh
+++ .deploy/scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 docker run -d --name $HEROKU_APP_NAME -e "PORT=9876" -p 9876:9876 ${HEROKU_REGISTRY_IMAGE};
-docker exec -it $HEROKU_APP_NAME php -i | grep "PHP Version => 7.4.5"
+docker exec -it $HEROKU_APP_NAME php -i | grep "PHP Version => 7.4.6"
 docker inspect -f {{.Config.Labels}} $HEROKU_APP_NAME
 docker inspect -f {{.Config.Labels}} $HEROKU_APP_NAME | grep $VCS_REF
